e6520b13e9086d22bc7f61f6d1dd30795ce7725c
jihun <jihunv8@gmail.com>
2022-08-31 11:17:58 +0900

    [add] style-util 추가

---
+++ src/global-style/style-util/mediaSize.js
@@ -0,0 +1,10 @@
+export const contentsAreaXSmall = ({ theme }) => theme.mediaSize.contentsArea.xSmall;
+export const contentsAreaSmall = ({ theme }) => theme.mediaSize.contentsArea.small;
+export const contentsAreaMiddle = ({ theme }) => theme.mediaSize.contentsArea.middle;
+export const contentsAreaLarge = ({ theme }) => theme.mediaSize.contentsArea.large;
+export const contentsAreaXLarge = ({ theme }) => theme.mediaSize.contentsArea.xLarge;
+
+export const screenXSmall = ({ theme }) => theme.mediaSize.screen.xSmall;
+export const screenSmall = ({ theme }) => theme.mediaSize.screen.small;
+export const screenMiddle = ({ theme }) => theme.mediaSize.screen.middle;
+export const screenLarge = ({ theme }) => theme.mediaSize.screen.large;
